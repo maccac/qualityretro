c1076fde4f673fc137fa4974869ff85d9989f384
Alexander Constanti and Marcel Caroly <just-dev+aconstanti+mcaroly@tyro.com>
2017-03-16 17:07:32 +1100

    'Fixed' flashing page stuff code blitz stylz.

--- src/main/resources/webroot/page/home/chart-component.js
+++ src/main/resources/webroot/page/home/chart-component.js
@@ -3,16 +3,10 @@ function ChartController($scope) {
 
     vm.metric = vm.metric || {};
 
-    console.log(vm.metric);
-
     vm.colors = ["rgb(154,50,30)", "rgb(255,204,0)", "rgb(50,204,40)"];
     vm.labels = ['Latest', '2017-01-01'];
     vm.series = ["Green", "Yellow", "Red"];
-    vm.data = [
-        [vm.metric.redIndicators, 1], // red
-        [vm.metric.yellowIndicators, 1], // yellow
-        [vm.metric.greenIndicators, 5] // green
-    ];
+    setChartData();
 
     vm.options = {
         scales: {
@@ -22,12 +16,17 @@ function ChartController($scope) {
         }
     };
 
-    $scope.$watch('vm.metric', function () {
-        vm.data = [
+    function setChartData() {
+        if (vm.metric.hasOwnProperty("redIndicators")) {
+            vm.data = [
                 [vm.metric.redIndicators, 1], // red
                 [vm.metric.yellowIndicators, 1], // yellow
                 [vm.metric.greenIndicators, 5] // green
             ];
+        }
+    }
+    $scope.$watch('vm.metric', function () {
+        setChartData();
     });
 
 }
