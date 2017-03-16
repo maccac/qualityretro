47b9731e97c9fe94e7c1c1c7f97e4d882f4ed267
Alexander Constanti and Marcel Caroly <just-dev+aconstanti+mcaroly@tyro.com>
2017-03-17 08:56:22 +1100

    Updated axis to be hidden

--- src/main/resources/webroot/page/home/chart-component.js
+++ src/main/resources/webroot/page/home/chart-component.js
@@ -4,14 +4,18 @@ function ChartController($scope) {
     vm.metric = vm.metric || {};
 
     vm.colors = ["rgb(154,50,30)", "rgb(255,204,0)", "rgb(50,204,40)"];
-    vm.labels = ['Latest', '2017-01-01'];
+    vm.labels = [''];
     vm.series = ["Green", "Yellow", "Red"];
     setChartData();
 
     vm.options = {
         scales: {
             yAxes: [{
-                stacked: true
+                stacked: true,
+                display: false
+            }],
+            xAxes: [{
+                display: false
             }]
         }
     };
@@ -19,9 +23,9 @@ function ChartController($scope) {
     function setChartData() {
         if (vm.metric.hasOwnProperty("redIndicators")) {
             vm.data = [
-                [vm.metric.redIndicators, 1], // red
-                [vm.metric.yellowIndicators, 1], // yellow
-                [vm.metric.greenIndicators, 5] // green
+                [vm.metric.redIndicators], // red
+                [vm.metric.yellowIndicators], // yellow
+                [vm.metric.greenIndicators] // green
             ];
         }
     }
