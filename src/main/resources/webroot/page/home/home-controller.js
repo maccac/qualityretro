acbfe407796f0eb77f74cb1346b18e5ee57e42b0
Alexander Constanti and Marcel Caroly <just-dev+aconstanti+mcaroly@tyro.com>
2017-03-17 10:05:44 +1100

    Dynamicafied nav bar

--- src/main/resources/webroot/page/home/home-controller.js
+++ src/main/resources/webroot/page/home/home-controller.js
@@ -1,9 +1,15 @@
-angular.module("quality-retro").controller("homeCtrl", function ($scope, MetricsService) {
+angular.module("quality-retro").controller("homeCtrl", function ($scope, $rootScope, MetricsService) {
     var vm = this;
     $scope.vm = vm;
 
     MetricsService.getMetrics("latest").$promise
         .then(function (data) {
+            $rootScope.team = {
+                teamName: data.teamName,
+                teamLogo: data.teamLogo,
+                teamId: data.teamId,
+                date: data.date
+            };
             vm.metrics = data;
         });
 });
