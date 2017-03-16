48bab1d62bd3d9182ca5aadbd7dd3e8cda6cb842
Alexander Constanti and Marcel Caroly <just-dev+aconstanti+mcaroly@tyro.com>
2017-03-16 13:01:23 +1100

    Initial Angular Gulp commit.

---
+++ Gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require("gulp"),
+    mainBowerFiles = require('main-bower-files'),
+    rename = require("gulp-rename"),
+    inject = require("gulp-inject");
+
+
+gulp.task("vendor:copy", [], function () {
+    return gulp.src(mainBowerFiles(), {read: true, "base" : "."})
+        .pipe(gulp.dest("src/main/resources/webroot/"));
+});
+
+gulp.task("vendor:inject", ["vendor:copy"], function () {
+    return gulp.src("src/main/resources/webroot/index.html")
+        .pipe(inject(gulp.src(mainBowerFiles(), {read: false}), {starttag: '<!-- inject:vendor:{{ext}} -->'}))
+        .pipe(rename("index.html"))
+        .pipe(gulp.dest("src/main/resources/webroot"));
+});
+
+gulp.task("default", ["vendor:inject"], function () {
+    return gulp.src("src/main/resources/webroot/index.html")
+        .pipe(inject(gulp.src(["src/main/resources/webroot/js/*.js"], {read: false}), {ignorePath: "/src/main/resources/webroot/"}))
+        .pipe(rename("index.html"))
+        .pipe(gulp.dest("src/main/resources/webroot"));
+});
---
+++ src/main/resources/webroot/js/app.js
@@ -0,0 +1,5 @@
+angular.module("quality-retro", []);
+
+angular.module("quality-retro").config(function ($route) {
+
+});
