e1def0e1abccd60f2235dfec7c3887978e12bb7c
Alexander Constanti and Marcel Caroly <just-dev+aconstanti+mcaroly@tyro.com>
2017-03-17 12:02:46 +1100

    Added cache busting to js files

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp"),
     mainBowerFiles = require('main-bower-files'),
     rename = require("gulp-rename"),
+    hash = require("gulp-hash"),
     inject = require("gulp-inject");
 
 
@@ -17,10 +18,17 @@ gulp.task("vendor:inject", ["vendor:copy"], function () {
 });
 
 gulp.task("default", ["vendor:inject"], function () {
+    var sourceJs = gulp.src(["!src/main/resources/webroot/bower_components/**/*.js", "src/main/resources/webroot/**/*.js"])
+            .pipe(hash({
+                template: '<%= name %><%= ext %>?v=<%= hash %>'
+            }))
+        // .pipe(gulp.dest("src/main/resources/webroot/dest"))
+        ;
+
     return gulp.src("src/main/resources/webroot/index.html")
-        .pipe(inject(
-            gulp.src(["!src/main/resources/webroot/bower_components/**/*.js", "src/main/resources/webroot/**/*.js"],
-                {read: false}), {ignorePath: "/src/main/resources/webroot/"}))
+        .pipe(inject(sourceJs, {
+            ignorePath: "/src/main/resources/webroot/"
+        }))
         .pipe(rename("index.html"))
         .pipe(gulp.dest("src/main/resources/webroot"));
 });
